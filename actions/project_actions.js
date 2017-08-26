79e810b4a20fb17e784bac12e3d75e45676dc099
NinoMaj <nino.majder@gmail.com>
2017-08-26 13:09:18 +0200

    Add SwipeScreen.

--- actions/project_actions.js
+++ actions/project_actions.js
@@ -23,25 +23,25 @@ export const fetchProjects = () => {
         postedBy: 'John Doe',
         createdDate: 'xx.xx.xxxx',
         techRequired: ['JS', 'ReactNative'],
-        skillsRequired: ['10 years of coding']
+        skillsRequired: ['10 years of coding'],
       },
       {
         id: 1,
         title: 'Some random project2',
         description: 'This is description of some random project.',
-        postedBy: 'John Doe'
-        createdDate: 'xx.xx.xxxx'
+        postedBy: 'John Doe',
+        createdDate: 'xx.xx.xxxx',
         techRequired: ['JS', 'ReactNative'],
-        skillsRequired: ['10 years of coding']
+        skillsRequired: ['10 years of coding'],
       },
       {
         id: 2,
         title: 'Some random project3',
         description: 'This is description of some random project.',
-        postedBy: 'John Doe'
-        createdDate: 'xx.xx.xxxx'
+        postedBy: 'John Doe',
+        createdDate: 'xx.xx.xxxx',
         techRequired: ['JS', 'ReactNative'],
-        skillsRequired: ['10 years of coding']
+        skillsRequired: ['10 years of coding'],
       },
     ],
   };
